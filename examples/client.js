f3870b099478aa8ac0a807546c08483cb564ed30
kyjackchan <kin.chan@smartparking.com>
2024-10-30 13:04:17 +1300

    update the ip address

--- examples/client.js
+++ examples/client.js
@@ -1,5 +1,5 @@
 const coap = require('../') // or coap
-const req = coap.request('coap://localhost/Kin')
+const req = coap.request('coap://34.151.64.135/Kin')
 
 req.on('response', (res) => {
     res.pipe(process.stdout)
